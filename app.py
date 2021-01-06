a8c19bb7405f6379188b06ba0935bd08c312ed39
Mikkel Jeppesen <2756925+Duckle29@users.noreply.github.com>
2021-01-06 20:18:31 +0100

    Moved to config parser and support for multiple sites

--- app.py
+++ app.py
@@ -5,35 +5,40 @@ from werkzeug.middleware.proxy_fix import ProxyFix
 from werkzeug.exceptions import InternalServerError
 
 import subprocess
-import json
+from configparser import ConfigParser
 
-config = {}
-with open('config.json') as json_file:
-    config = json.load(json_file)
+config = ConfigParser()
+config.read("config.ini")
 
 app = Flask(__name__)
 app.wsgi_app = ProxyFix(app.wsgi_app, x_for=1, x_proto=1)
 
-app.config['GITHUB_WEBHOOKS_KEY'] = config['gh_secret']
+app.config['GITHUB_WEBHOOKS_KEY'] = config['main']['github_secret']
 app.config['VALIDATE_IP'] = True
 app.config['VALIDATE_SIGNATURE'] = True
 
-hooks_deploy_mikkel_cc = Hooks(app, url=config['mikkel.cc']['endpoint'])
 
+for site in config:
+    if site in ['DEFAULT', 'main']: 
+        continue
+    
+    print(site)
 
-@hooks_deploy_mikkel_cc.hook('push')
-def website_deploy(data, guid):
+deploy_hook = Hooks(app, url=config['main']['endpoint'])
+
+@deploy_hook.hook('push')
+def deploy(data, guid):
     repo = data['repository']['full_name']
     branch = data['ref'].split('/')[-1]
     master_branch = data['repository']['master_branch']
 
-    if repo != 'Duckle29/startbootstrap-resume':
+    if repo not in config:
         return 'Nothing to do for this repo, skipped.'
     if branch != master_branch:
         return 'Not production branch, skipped.'
 
     try:
-        subprocess.run(['/usr/bin/git',  'pull'], cwd=config['mikkel.cc']['path'], check=True)
+        subprocess.run(['/usr/bin/git',  'pull'], cwd=config[repo]['path'], check=True)
     except subprocess.CalledProcessError as err:
         raise InternalServerError(err)
 
